8f8acb26173c043a47c8b82e868eb052a9127be8
Martine Yemma <70291070+mylaconcepts@users.noreply.github.com>
2021-01-24 15:23:28 -0800

    Finished product
    created new 'active' class function for eventlistener
    
    added scroll to section function

--- projects/landing-page/app.js
+++ projects/landing-page/app.js
@@ -20,12 +20,12 @@
 const navElements = document.querySelectorAll('section')
 const navBar = document.getElementById('navbar__list')
 
+
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -33,38 +33,18 @@ const navBar = document.getElementById('navbar__list')
 */
 
 // build the nav
-navElements.forEach(function(section){
+navElements.forEach( section => {
  const navlistElement = `<li class='menu__link ${section.className}' data-link=${section.id}><a href="#${section.id}">${section.dataset.nav}</li>`
  navBar.insertAdjacentHTML('beforeend',navlistElement)
 })
 
 // Add class 'active' to section when near top of viewport
-
-
-const callback = entries => {
-  entries.forEach(entry => {
-    const navListElement = document.querySelector(`.menu__link[data-link='${entry.target.id}']`,)
-
-    const section = document.getElementById(entry.target.id)
-
-    .addEventListener('click', function() {
-
-    if (entry && entry.isIntersecting) {
-      navListElement.classList.add('active')
-      section.classList.add('active')
-    } else {
-      if (navListElement.classList.contains('active')) {
-        navListElement.classList.remove('active')
-      }
-
-      if (section.classList.contains('active')) {
-        section.classList.remove('active')
-      }
-    }
-  }
-  })
-}
-
+navBar.addEventListener('click', e => {
+  
+  e.preventDefault();
+  navBar.querySelector('.active')?.classList.remove('active');
+  e.target.classList.add('active');
+});
 /**
  * End Main Functions
  * Begin Events
@@ -77,10 +57,17 @@ const callback = entries => {
 navBar.addEventListener('click', e => {
   e.preventDefault()
   const parent = e.target.hasAttribute('data-link')
-    ? e.target
-    : e.target.parentElement
+  ? e.target
+  : e.target.parentElement
   const elementToScrollTo = document.getElementById(parent.dataset.link)
   elementToScrollTo.scrollIntoView({block: 'end', behavior: 'smooth'})
 })
 
 // Set sections as active
+
+/*Add an active state to your navigation items when a section is in the viewport.
+Hide fixed navigation bar while not scrolling (it should still be present on page load).
+Hint: setTimeout can be used to check when the user is no longer scrolling.
+Add a scroll to top button on the page that’s only visible when the user scrolls below the fold of the page.
+Update/change the design/content.
+Make sections collapsible.*/
